bb2e8a2ce72d380f09afd24826c2c42d33e8bdc0
Facundo Andrean <facundoandrean22@gmail.com>
2025-06-30 23:50:22 -0300

    chore: configurando la estructura de carpetas del backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (_req, res) => {
+  res.send("Hola");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+export default app;
