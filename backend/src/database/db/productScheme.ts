208a299ec9483894c6520ae716a33c8c009f0c0f
Facundo Andrean <facundoandrean22@gmail.com>
2025-07-01 13:50:42 -0300

    feat: mapea la entidad products

---
+++ backend/src/database/db/productScheme.ts
@@ -0,0 +1,12 @@
+import { sql } from "drizzle-orm";
+import { integer, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const products = sqliteTable("products", {
+  product_id: text("product_id").primaryKey().notNull(),
+  name: text("name").notNull(),
+  description: text("description"),
+  price: real("price").notNull(),
+  stock: integer("stock").notNull(),
+  created_at: text("created_at").notNull().default(sql`CURRENT_TIMESTAMP`),
+  updated_at: text("updated_at").notNull().default(sql`CURRENT_TIMESTAMP`),
+})
