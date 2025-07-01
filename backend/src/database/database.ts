a6ee065e4a5a9a27f506d6df74d3a786a23e4989
Facundo Andrean <facundoandrean22@gmail.com>
2025-07-01 12:00:31 -0300

    feat: conecta el backend a la base de datos

---
+++ backend/src/database/database.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client";
+import config from "../config";
+
+const client = createClient({
+  url: config.urlDB as string,
+  authToken: config.tokenDB as string,
+})
+
+export const db = drizzle({ client });
