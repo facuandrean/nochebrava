1d4290f1a4b6b9cbb6059d627c0e5d80ff2b49a9
Facundo Andrean <facundoandrean22@gmail.com>
2025-07-06 17:34:33 -0300

    feat: crea crud para la entidad paymentMethod

--- backend/src/controllers/paymentMethodController.ts
+++ backend/src/controllers/paymentMethodController.ts
@@ -1,19 +1,164 @@
 import type { Request, Response } from "express";
+import { AppError } from "../errors";
+import { paymentMethodService } from "../services/paymentMethodService";
+import type { PaymentMethod, PaymentMethodBodyPost } from "../types/types";
 
 const getAllPaymentMethods = async (_req: Request, res: Response): Promise<void> => {
+  try {
+    const paymentMethods: PaymentMethod[] = await paymentMethodService.getAllPaymentMethods();
 
+    if (paymentMethods.length === 0) {
+      res.status(404).json({
+        status: "Operación fallida.",
+        message: "No se encontraron métodos de pago.",
+        data: []
+      });
+      return;
+    }
+
+    res.status(200).json({
+      status: "Operación exitosa.",
+      message: "Métodos de pago obtenidos correctamente.",
+      data: paymentMethods
+    });
+    return;
+  } catch (error) {
+    if (error instanceof AppError) {
+      res.status(error.status).json({
+        status: "Operación fallida.",
+        message: error.message,
+        data: error.data
+      });
+      return;
+    }
+
+    res.status(500).json({
+      status: "Error interno del servidor.",
+      message: "Ocurrió un error al procesar la solicitud.",
+      data: []
+    });
+    return;
+  }
 };
 
 const getPaymentMethodById = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const payment_method_id = req.params.payment_method_id as string;
 
+    const paymentMethod: PaymentMethod | undefined = await paymentMethodService.getPaymentMethodById(payment_method_id);
+
+    if (!paymentMethod) {
+      res.status(404).json({
+        status: "Operación fallida.",
+        message: "No se encontró el método de pago.",
+        data: []
+      });
+      return;
+    }
+
+    res.status(200).json({
+      status: "Operación exitosa.",
+      message: "Método de pago obtenido correctamente.",
+      data: paymentMethod
+    });
+    return;
+
+  } catch (error) {
+    if (error instanceof AppError) {
+      res.status(error.status).json({
+        status: "Operación fallida.",
+        message: error.message,
+        data: error.data
+      });
+      return;
+    }
+
+    res.status(500).json({
+      status: "Error interno del servidor.",
+      message: "Ocurrió un error al procesar la solicitud.",
+      data: []
+    });
+    return;
+  }
 }
 
 const postPaymentMethod = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const dataPaymentMethod = req.body as PaymentMethodBodyPost;
 
+    const newPaymentMethod = {
+      ...dataPaymentMethod
+    };
+
+    const paymentMethod: PaymentMethod = await paymentMethodService.postPaymentMethod(newPaymentMethod);
+
+    res.status(201).json({
+      status: "Operación exitosa.",
+      message: "Método de pago creado correctamente.",
+      data: paymentMethod
+    });
+    return;
+
+  } catch (error) {
+    if (error instanceof AppError) {
+      res.status(error.status).json({
+        status: "Operación fallida.",
+        message: error.message,
+        data: error.data
+      });
+      return;
+    }
+
+    res.status(500).json({
+      status: "Error interno del servidor.",
+      message: "Ocurrió un error al procesar la solicitud.",
+      data: []
+    });
+    return;
+  }
 }
 
 const deletePaymentMethod = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const payment_method_id = req.params.payment_method_id as string;
 
+    const paymentMethod: PaymentMethod | undefined = await paymentMethodService.getPaymentMethodById(payment_method_id);
+
+    if (!paymentMethod) {
+      res.status(404).json({
+        status: "Operación fallida.",
+        message: "No se encontró el método de pago.",
+        data: []
+      });
+      return;
+    }
+
+    await paymentMethodService.deletePaymentMethod(payment_method_id);
+
+    res.status(204).json({
+      status: "Operación exitosa.",
+      message: "Método de pago eliminado correctamente.",
+      data: []
+    });
+    return;
+    
+  } catch (error) {
+    if (error instanceof AppError) {
+      res.status(error.status).json({
+        status: "Operación fallida.",
+        message: error.message,
+        data: error.data
+      });
+      return;
+    }
+
+    res.status(500).json({
+      status: "Error interno del servidor.",
+      message: "Ocurrió un error al procesar la solicitud.",
+      data: []
+    });
+    return;
+  }
 }
 
 export const paymentMethodController = {
--- backend/src/services/paymentMethodService.ts
+++ backend/src/services/paymentMethodService.ts
@@ -1,21 +1,51 @@
+import { eq } from "drizzle-orm";
+import { db } from "../database/database";
+import { paymentMethods } from "../database/db/paymentMethodScheme";
+import { AppError } from "../errors";
 import type { PaymentMethod, PaymentMethodBodyPost } from "../types/types";
+import { v4 as uuid } from "uuid";
 
 const getAllPaymentMethods = async (): Promise<PaymentMethod[]> => {
-  return [];
+  try {
+    const allPaymentMethods: PaymentMethod[] = await db.select().from(paymentMethods);
+    return allPaymentMethods;
+  } catch (error) {
+    throw new AppError("Ocurrió un error al obtener los métodos de pago.", 400, []);
+  }
 };
 
 const getPaymentMethodById = async (payment_method_id: string): Promise<PaymentMethod | undefined> => {
-  return undefined;
+  try {
+    const paymentMethod: PaymentMethod | undefined = await db.select().from(paymentMethods).where(eq(paymentMethods.payment_method_id, payment_method_id)).get();
+    return paymentMethod;
+  } catch (error) {
+    throw new AppError("Ocurrió un error al obtener el método de pago.", 400, []);
+  }
 }
 
-const postPaymentMethod = async (dataPaymentMethod: PaymentMethodBodyPost): Promise<void> => {
+const postPaymentMethod = async (dataPaymentMethod: PaymentMethodBodyPost): Promise<PaymentMethod> => {
+  try {
+    const newPaymentMethod = {
+      payment_method_id: uuid(),
+      ...dataPaymentMethod
+    };
+
+    const paymentMethod: PaymentMethod = await db.insert(paymentMethods).values(newPaymentMethod).returning().get();
+    return paymentMethod;
+  } catch (error) {
+    throw new AppError("Ocurrió un error al crear el método de pago.", 400, []);
+  }
 }
 
 const deletePaymentMethod = async (payment_method_id: string): Promise<void> => {
-
+  try {
+    await db.delete(paymentMethods).where(eq(paymentMethods.payment_method_id, payment_method_id));
+  } catch (error) {
+    throw new AppError("Ocurrió un error al eliminar el método de pago.", 400, []);
+  }
 }
 
-export const paymentMethodController = {
+export const paymentMethodService = {
   getAllPaymentMethods,
   getPaymentMethodById,
   postPaymentMethod,
