3352d544d5766364692ca77fa772a26ff24c834a
Facundo Andrean <facundoandrean22@gmail.com>
2025-07-17 12:10:15 -0300

    feat: agrega boton de hamburguesa para desplegar menu en versión mobile

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import { Button } from "../button/button";
 import "./header.css";
 
 export const Header = () => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuOpen = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
 
   const handleRegisterSale = () => {
     console.log("Registrar venta");
@@ -12,8 +19,15 @@ export const Header = () => {
       <div>
         <span>Noche Brava</span>
       </div>
-      <div>
+      <div className="header-registerSale">
         <Button label="Registrar venta" parentMethod={handleRegisterSale} />
+      </div>
+      <div className="header-menu">
+        <button className={`hamburger hamburger--elastic ${isMenuOpen ? "is-active" : ""}`} type="button" onClick={handleMenuOpen}>
+          <span className="hamburger-box">
+            <span className="hamburger-inner"></span>
+          </span>
+        </button>
       </div>
     </header>
   );
