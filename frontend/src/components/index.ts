ea91c0ed92369a2457eae30b216f136b8ab7ed1c
Facundo Andrean <facundoandrean22@gmail.com>
2025-07-17 14:04:37 -0300

    feat: corrige barril de importaciones

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,6 +1,7 @@
 // La idea es que este archivo sea el que exporte todos los componentes que se van a usar en la aplicación, para simplificar el import de los mismos
 // Voy a tener tantos export * como componentes tenga el proyecto
 export * from "./button/button.tsx";
+export * from "./header/header.tsx";
 
 // para importar un componente en particular, se puede hacer de la siguiente manera:
 // import { Button, Button2 } from "./components"; y asi sucesivamente dependiendo de los export y componentes que tenga el proyecto.
